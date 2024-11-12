09377157c2137f79b8a1fa73ade82c00bd9e960c
IftekarRahmanRuhit <iftekarrahmanruhit2003@gmail.com>
2024-11-12 21:56:20 +0600

    added navbar

--- src/Components/Root.jsx
+++ src/Components/Root.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Navabar from './Navabar'
 
 const Root = () => {
   return (
     <div>
-
+        <Navabar></Navabar>
         <Outlet></Outlet>
       
 
